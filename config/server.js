f8009f5e972e495317c54255307d79d83a6d0c48
Samuel Almeida <samuelalmeida.dev@gmail.com>
2017-10-29 20:53:32 -0300

    Express Validator

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views' , './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
     .include('app/routes')
