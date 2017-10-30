7bd7d9a059dcd174a9b1a50644e3728ba67ebd42
Samuel Almeida <samuelalmeida.dev@gmail.com>
2017-10-29 23:22:23 -0300

    Validação de formulário com express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,15 +1,31 @@
 module.exports = (app) => {
     app.get('/formulario-noticia', (req, res) => {
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao: {}, noticia: {}});
     });
 
     app.post('/noticia/salvar', (req, res) => {
         let noticia = req.body;
 
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', "Resumo é obrigatório").notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor','Autor é obrigatório').notEmpty();
+        req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'Notícia é obrigatório').notEmpty();
+
+        let errors = req.validationErrors();
+
+        if(errors){
+            res.render("admin/form_add_noticia", {validacao: errors, noticia: noticia});
+            return null;
+        }
+
         const connection = app.config.dbConnection();
         const noticiasDAO = new app.app.models.NoticiasDAO(connection);
+        console.log('teste');
         
         noticiasDAO.salvarNoticia(noticia, (error, success) => {
+            
            res.redirect('/noticias');
         });
     });
